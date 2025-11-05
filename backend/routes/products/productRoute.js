ea5c59514995ce5158b8b45570da919e3a3ca484
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-11-05 17:47:36 +0530

    Delete backend directory

--- backend/routes/products/productRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../../controllers/recommendation.controller');
-
-// GET /api/products?defect=some_defect
-router.get('/recommendations/:defect', productController.getProductsByDefect);
-
-module.exports = router;
--- backend/utils/bufferConverter.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-async function urlToBuffer(imageUrl) {
-  const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-  return Buffer.from(response.data);
-}
-
-module.exports = urlToBuffer;
